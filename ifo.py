e1cee2a74d9e4a486c33bf1036239d7a0532b8ba
jcvsilva <jcvsilva@gmail.com>
2020-11-15 17:42:29 +0000

    Folder cleaning and args bug.

--- ifo.py
+++ ifo.py
@@ -5,23 +5,26 @@ import os
 import argparse
 
 # Create a parser for args
-parser = argparse.ArgumentParser(description='Process torrents RAR\'s.')
+parser = argparse.ArgumentParser(description='Organize RAW files.')
 
 # Add an argument
-parser.add_argument('dir_arg', help='Directório a processar.')
+parser.add_argument("-d", dest="dir_arg", default=".", help='Folder to process. (under development)')
 
 # Parse arguments
 args = parser.parse_args()
 
 print(args.dir_arg)
 
+# Get list of files/folders in current folder 
 dir_list = os.listdir(".")
 
+# Loop current folder
 for folder in dir_list:
     if os.path.isdir(folder):
         sub_dir_list = os.listdir(os.path.join(".", folder))
         # print(f'List sub: {sub_dir_list}')
         
+        # Check if there is a RAW folder
         for image in sub_dir_list:
             try:
                 os.mkdir(os.path.join(".", folder, "RAW"))
